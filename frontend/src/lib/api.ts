5f915c13b30811f4269f2ca5cb4192c773002772
Dhruvil Rangani <dhruvilrangani007@gmail.com>
2025-06-11 01:51:35 -0400

    cors fixed

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api',
+    baseURL: 'https://express-doc-annotator.onrender.com/api',
     headers: {
         'Content-Type': 'application/json',
     },
